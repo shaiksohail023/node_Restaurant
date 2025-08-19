02022c0c4632097c24fdd5df3b49af5577269d8a
shaiksohail <81278891+shaiksohail023@users.noreply.github.com>
2025-08-19 16:09:23 +0530

    added some routes in MenuRoute

--- routes/menuItemRoutes.js
+++ routes/menuItemRoutes.js
@@ -28,5 +28,64 @@ router.get('/', async(req,res)=>{
     }
 });
 
+//parameterized call
+router.get('/:taste', async(req,res)=>{
+    try{
+        const taste = req.params.taste;
+        if(taste == 'spicy' || taste == 'sweet' || taste == 'sour'){
+            const response = await Person.find({taste:taste});
+            console.log('Data Fetched Successfully');
+            res.status(200).json(response);
+        }else{
+            res.status(404).json({error: 'Invalid Taste'});
+        }
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
+//update person
+router.put('/:id', async(req,res)=>{
+    try{
+        const menuId = req.params.id; //Extract the id from URL Parameter
+        const updatedMenuItemData = req.body;
+
+        const response = await Person.findByIdAndUpdate(menuId,updatedMenuItemData, {
+            new: true, //return updated document
+            runValidators: true //Run mongoose validation
+        })
+
+        //if the id is not present in person table then it will go in these 
+        // if condition
+        if(!response){
+            return res.status(400).json({error: 'Menu Item not found'});
+        }
+
+        console.log('data Updated Successfully');
+        res.status(200).json(response);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
+router.delete('/:id', async(req,res)=>{
+    try{
+        const menuId = req.params.id; //Extract the id from URL Parameter
+        const response = await Person.findByIdAndDelete(menuId);
+
+        if(!response){
+            return res.status(400).json({error: 'MenuItem not found'});
+        }
+
+        console.log('Record deleted Successfully');
+        res.status(200).json({message: 'MenuItem deleted successfully'});
+    }catch(error){
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
 // commented
 module.exports = router;
